4d53bfd61415840c815d91d0fbf886f69eaddec7
Boris Kossev <bkossev@aservo.com>
2017-10-13 17:18:30 +0200

    r

--- vault-shared-lib.groovy
+++ vault-shared-lib.groovy
@@ -11,7 +11,7 @@ def GenerateTokenHash(
     try{
           sh "echo ${VaultServerURL}"
           sh "echo ${VaultTokenTTL}"
-          sh '''curl -sSL --header "Content-Type: application/json" --header "X-Vault-Token: 696d336f-610f-1529-7e17-888eb423fbfa" --request POST --data '{ "policies": [ "policy_devel_ro_jira" ], "ttl": "24h" }' http://vault.10.50.10.1.xip.io:8200/v1/auth/token/create '''
+          sh '''curl -sSL --header "Content-Type: application/json" --header "X-Vault-Token: c35c99d3-cf78-6fb3-b604-7d390abab983" --request POST --data '{ "policies": [ "policy_devel_ro_jira" ], "ttl": "24h" }' http://vault.10.50.10.1.xip.io:8200/v1/auth/token/create '''
 
        } catch(err) {
             echo "error"

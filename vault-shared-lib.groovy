c66bfcf7546544aa04608298baf109f4c95445a9
Boris Kossev <bkossev@aservo.com>
2017-10-13 18:05:48 +0200

    e

--- vault-shared-lib.groovy
+++ vault-shared-lib.groovy
@@ -10,9 +10,9 @@ def GenerateTokenHash(
 ){
     try{
           sh "echo \"test\" ${VaultDevel2MasterROToken}"
-          sh "curl -sSL --header \"Content-Type: application/json\" --header \"X-Vault-Token: ${VaultDevel2MasterROToken}\" --request POST --data '{ \"policies\": [ \"${VaultTokenPolicy}\" ], \"ttl\": \"${VaultTokenTTL}\" }' ${VaultServerURL}/v1/auth/token/create  | python -c \"import sys, json; print json.load(sys.stdin)['auth']['client_token']\" | base64"
-          //sh "curl -sSL --header \"Content-Type: application/json\" --header \"X-Vault-Token: ${VaultDevel2MasterROToken}\" --request POST --data '{ \\"policies\\": [ \\"${VaultTokenPolicy}\\" ], \\"ttl\\": \\"${VaultTokenTTL}\\" }' ${VaultServerURL}/v1/auth/token/create"
-
+          sh "curl -sSL --header \"Content-Type: application/json\" --header \"X-Vault-Token: ${VaultDevel2MasterROToken}\" --request POST --data '{ \"policies\": [ \"${VaultTokenPolicy}\" ], \"ttl\": \"${VaultTokenTTL}\" }' ${VaultServerURL}/v1/auth/token/create  | python -c \"import sys, json; print json.load(sys.stdin)['auth']['client_token']\" | base64 > > outFile"
+          a = readFile 'outFile'
+          echo "The current date is ${a}"
        } catch(err) {
             echo "error"
        }

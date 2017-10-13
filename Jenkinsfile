f1ee2ac7769fcb750e2ae88f2b869a81b00c0304
Boris Kossev <bkossev@aservo.com>
2017-10-13 17:03:39 +0200

    dscf

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,8 +5,8 @@ node {
 
     Vault = load 'test-shared-lib/vault-shared-lib.groovy'
     Kube = load 'test-shared-lib/kube-shared-lib.groovy'
-    
-    Vault.GenerateTokenHash('devel2')
+
+    Vault.GenerateTokenHash('devel2','wsddwd','346erdgfd','89324jkdkjdsf87234,dsflkjljksfd')
     Kube.WriteTokenSecret('devel2')
 
     deleteDir()
--- vault-shared-lib.groovy
+++ vault-shared-lib.groovy
@@ -1,5 +1,19 @@
-def GenerateTokenHash (args) {
+def GenerateTokenHash0 (args) {
       sh "echo Hallo ${args}"
+}
+
+def GenerateTokenHash(
+    String VaultServerURL,
+    String VaultTokenTTL,
+    String VaultTokenPolicy,
+    String VaultDevel2MasterROToken
+){
+    try{
+          sh "echo ${VaultServerURL}"
+          sh "echo ${VaultTokenTTL}"
+       } catch(err) {
+            echo "error"
+       }
 }
 
 return this;

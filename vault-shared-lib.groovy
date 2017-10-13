da220d14f7ff94a86158223312dc833e3d70e88b
Boris Kossev <bkossev@aservo.com>
2017-10-13 17:54:16 +0200

    o

--- vault-shared-lib.groovy
+++ vault-shared-lib.groovy
@@ -9,10 +9,9 @@ def GenerateTokenHash(
     String VaultDevel2MasterROToken
 ){
     try{
-          sh '
-            echo "${VaultDevel2MasterROToken}"
-            curl -sSL --header "Content-Type: application/json" --header "X-Vault-Token: ${VaultDevel2MasterROToken}" --request POST --data '{ \\"policies\\": [ \\"${VaultTokenPolicy}\\" ], \\"ttl\\": \\"${VaultTokenTTL}\\" }' ${VaultServerURL}/v1/auth/token/create
-          '
+          sh "echo ${VaultDevel2MasterROToken}"
+          sh "curl -sSL --header "Content-Type: application/json" --header "X-Vault-Token: ${VaultDevel2MasterROToken}" --request POST --data '{ \\"policies\\": [ \\"${VaultTokenPolicy}\\" ], \\"ttl\\": \\"${VaultTokenTTL}\\" }' ${VaultServerURL}/v1/auth/token/create"
+
        } catch(err) {
             echo "error"
        }

da92b109d7539500284bef2d9c01f027d0dca611
Boris Kossev <bkossev@aservo.com>
2017-10-13 17:23:26 +0200

    f

--- vault-shared-lib.groovy
+++ vault-shared-lib.groovy
@@ -10,7 +10,7 @@ def GenerateTokenHash(
 ){
     try{
           sh "echo ${VaultServerURL}"
-          sh "echo ${VaultTokenTTL}"
+          sh "echo ${VaultDevel2MasterROToken}"
           sh '''curl -sSL --header "Content-Type: application/json" --header "X-Vault-Token: ${VaultDevel2MasterROToken}" --request POST --data '{ "policies": [ "policy_devel_ro_jira" ], "ttl": "24h" }' http://vault.10.50.10.1.xip.io:8200/v1/auth/token/create '''
 
        } catch(err) {

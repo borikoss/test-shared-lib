c965826da60914ed8e3ae711ed26d7767a758c23
Boris Kossev <bkossev@aservo.com>
2017-10-13 17:46:27 +0200

    d

--- vault-shared-lib.groovy
+++ vault-shared-lib.groovy
@@ -9,7 +9,7 @@ def GenerateTokenHash(
     String VaultDevel2MasterROToken
 ){
     try{
-          sh '''curl -sSL --header "Content-Type: application/json" --header "X-Vault-Token: ${VaultDevel2MasterROToken}" --request POST --data '{ \\"policies\\": [ \\"${VaultTokenPolicy}\\" ], \\"ttl\\": \\"${VaultTokenTTL}\\" }' ${VaultServerURL}/v1/auth/token/create | python -c "import sys, json; print json.load(sys.stdin)['auth']['client_token']" | base64 > outFile '''
+          sh '''curl -sSL --header "Content-Type: application/json" --header "X-Vault-Token: ${VaultDevel2MasterROToken}" --request POST --data '{ \\"policies\\": [ \\"${VaultTokenPolicy}\\" ], \\"ttl\\": \\"${VaultTokenTTL}\\" }' ${VaultServerURL}/v1/auth/token/create > outFile '''
           a = readFile 'outFile'
           echo "The current date is ${a}"
 

a0d4fa4c6a31bf12d1e8f236fc6b465d7034ad57
Boris Kossev <bkossev@aservo.com>
2017-10-16 08:20:41 +0200

    de

--- vault-shared-lib.groovy
+++ vault-shared-lib.groovy
@@ -12,7 +12,8 @@ def GenerateTokenHash(
           sh "echo \"test\" ${VaultDevel2MasterROToken}"
           sh "curl -sSL --header \"Content-Type: application/json\" --header \"X-Vault-Token: ${VaultDevel2MasterROToken}\" --request POST --data '{ \"policies\": [ \"${VaultTokenPolicy}\" ], \"ttl\": \"${VaultTokenTTL}\" }' ${VaultServerURL}/v1/auth/token/create  | python -c \"import sys, json; print json.load(sys.stdin)['auth']['client_token']\" | base64 > outFile"
           VaultTokenHash = readFile 'outFile'
-          // echo "The current date is ${a}"
+          deleteDir()
+          
        } catch(err) {
             echo "error"
        }

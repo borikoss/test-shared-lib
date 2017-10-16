f8a934e1e92892c536a3a5cf20d33683627aa5b2
Boris Kossev <bkossev@aservo.com>
2017-10-16 09:23:00 +0200

    curl max time added

--- vault-shared-lib.groovy
+++ vault-shared-lib.groovy
@@ -6,7 +6,7 @@ def GenerateTokenHash(
 ){
     try{
           sh "unset http_proxy; unset https_proxy"
-          sh "curl -sSL --header \"Content-Type: application/json\" --header \"X-Vault-Token: ${VaultDevel2MasterROToken}\" --request POST --data '{ \"policies\": [ \"${VaultTokenPolicy}\" ], \"ttl\": \"${VaultTokenTTL}\" }' ${VaultServerURL}/v1/auth/token/create  | python -c \"import sys, json; print json.load(sys.stdin)['auth']['client_token']\" | base64 > outFile"
+          sh "curl -sSL --max-time 5 --header \"Content-Type: application/json\" --header \"X-Vault-Token: ${VaultDevel2MasterROToken}\" --request POST --data '{ \"policies\": [ \"${VaultTokenPolicy}\" ], \"ttl\": \"${VaultTokenTTL}\" }' ${VaultServerURL}/v1/auth/token/create  | python -c \"import sys, json; print json.load(sys.stdin)['auth']['client_token']\" | base64 > outFile"
           VaultTokenHash = readFile 'outFile'
           deleteDir()
 

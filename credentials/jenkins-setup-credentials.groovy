e46426f77001eda0698fc9a3dc20e189350c47ac
Boris Kossev <bkossev@aservo.com>
2017-10-19 09:38:19 +0200

    works on console

--- credentials/jenkins-setup-credentials.groovy
+++ credentials/jenkins-setup-credentials.groovy
@@ -4,7 +4,13 @@ import com.cloudbees.plugins.credentials.common.*
 import com.cloudbees.plugins.credentials.domains.*
 import com.cloudbees.plugins.credentials.impl.*
 import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
-import hudson.plugins.sshslaves.*;
+import org.jenkinsci.plugins.plaincredentials.*
+import org.jenkinsci.plugins.plaincredentials.impl.*
+import hudson.util.Secret
+import hudson.plugins.sshslaves.*
+import org.apache.commons.fileupload.*
+import org.apache.commons.fileupload.disk.*
+import java.nio.file.Files
 
 domain = Domain.global()
 store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
@@ -15,7 +21,7 @@ CredentialsScope.GLOBAL,
 "root",
 new BasicSSHUserPrivateKey.UsersPrivateKeySource(),
 "",
-""
+"Description comes here"
 )
 
 usernameAndPassword = new UsernamePasswordCredentialsImpl(
@@ -25,5 +31,12 @@ usernameAndPassword = new UsernamePasswordCredentialsImpl(
   "jenkins"
 )
 
+secretText = new StringCredentialsImpl(
+CredentialsScope.GLOBAL,
+"vault-devel2-jira-ro-token",
+"vault-devel2-jira-ro-token",
+Secret.fromString("1234567890"))
+
 store.addCredentials(domain, priveteKey)
 store.addCredentials(domain, usernameAndPassword)
+store.addCredentials(domain, secretText)
